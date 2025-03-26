5ee5901b31f360472c31ce1a6bf5df266a30a706
srijansa <159839387+srijansa@users.noreply.github.com>
2025-03-26 01:51:52 -0400

    Open functionality working + forcefully adding media jar file

--- VideoSnipper/src/main/java/module-info.java
+++ VideoSnipper/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.video.videosnipper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.media;
 
 
     opens org.video.videosnipper to javafx.fxml;
--- VideoSnipper/src/main/java/org/video/videosnipper/VideoController.java
+++ VideoSnipper/src/main/java/org/video/videosnipper/VideoController.java
@@ -1,8 +1,29 @@
 package org.video.videosnipper;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.media.*;
+import javafx.stage.FileChooser;
+//import javafx.;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class VideoController {
+    private String filePath;
+    private MediaPlayer mediaPlayer;
+    @FXML
+    private MediaView mediaView;
+    public void handleOpenAction(javafx.event.ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Video Files", "*.mp4");
+        fileChooser.getExtensionFilters().add(filter);
+        File file = fileChooser.showOpenDialog(null);
+        filePath = file.toURI().toString();
+        if (filePath != null){
+            Media media = new Media(filePath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.play();
+        }
 
+    }
 }
